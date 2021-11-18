1ff818f9addd11f435ba66f55235d56857712e01
Carl <cboettig@gmail.com>
2021-11-18 22:48:31 +0000

    online

--- R/test.R
+++ R/test.R
@@ -13,8 +13,8 @@ test <- function(){
   contentid:::sources_tsv(id)
   
   registries <- c(
-  "/home/cboettig/.local/share/R/contentid/registry.tsv",
-#  "https://hash-archive.org",                            
+#  "/home/cboettig/.local/share/R/contentid/registry.tsv",
+  "https://hash-archive.org",                            
 #  "https://hash-archive.carlboettiger.info",             
 #  "https://archive.softwareheritage.org",                
 #  "https://cn.dataone.org",                              
