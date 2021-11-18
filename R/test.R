85252347ae2a61d674872f0d4c48b06486f99649
Carl <cboettig@gmail.com>
2021-11-18 22:33:22 +0000

    test

--- R/test.R
+++ R/test.R
@@ -11,4 +11,42 @@ test <- function(){
   contentid:::sources_swh(id)
   contentid:::sources_store(id)
   contentid:::sources_tsv(id)
+  
+  registries <- c(
+  "/home/cboettig/.local/share/R/contentid/registry.tsv",
+#  "https://hash-archive.org",                            
+#  "https://hash-archive.carlboettiger.info",             
+#  "https://archive.softwareheritage.org",                
+#  "https://cn.dataone.org",                              
+#  "https://zenodo.org",                                  
+#  "/home/cboettig/.local/share/R/contentid"
+  )
+  
+  registries <- contentid:::expand_registry_urls(registries)
+  types <- contentid:::detect_registry_type(registries)
+  
+  active_registries <- registries[types == "content_store"]
+  out <- generic_source(id, 
+                        registries = active_registries, 
+                        type = "content_store")
+  
+  if (all(is.na(out$source)) | all) {
+    remote <- types[types %in% c(MIXED, REMOTES)]
+    remote_out <- lapply(
+      remote,
+      function(type) {
+        active_registries <- registries[types == type]
+        generic_source(id,
+          registries = active_registries,
+          type = type
+        )
+      }
+    )
+  
+    remote_out <- do.call(rbind, remote_out)
+    out <- rbind(out, remote_out)
+  }
+  # contentid:::filter_sources(out, registries, cols)
+  
+  
 }
