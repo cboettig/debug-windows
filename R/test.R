5946227ca4ea4a2758b0b15046a0a06b4788d3d4
Carl <cboettig@gmail.com>
2021-11-18 20:55:50 +0000

    a few remotes sources

--- R/test.R
+++ R/test.R
@@ -5,6 +5,9 @@
 test <- function(){
   id <- "hash://sha256/e17632191c0487128aa6d1ecb932b655157ad7eab26f2749c9797ce65bbbc130"
   #contentid::resolve(id)
-  contentid::sources(id)
+  contentid:::sources_ha(id)
+  contentid:::sources_dataone(id)
+  contentid:::sources_zenodo(id)
+  contentid:::sources_swh(id)
   
 }
