075b19c958d30de780886f739900dbc0000c469b
Carl <cboettig@gmail.com>
2021-11-18 20:09:53 +0000

    test

---
+++ R/test.R
@@ -0,0 +1,8 @@
+#' test
+#' @examples 
+#' test()
+#' @export
+test <- function(){
+  id <- "hash://sha256/e17632191c0487128aa6d1ecb932b655157ad7eab26f2749c9797ce65bbbc130"
+  contentid::resolve(id)
+}
