c0641a614bde06594a6a9563429a2faabf861db6
Carl <cboettig@gmail.com>
2021-11-18 22:52:52 +0000

    :hear_no_evil:

--- R/test.R
+++ R/test.R
@@ -14,12 +14,12 @@ test <- function(){
   
   registries <- c(
 #  "/home/cboettig/.local/share/R/contentid/registry.tsv",
-  "https://hash-archive.org",                            
-#  "https://hash-archive.carlboettiger.info",             
-#  "https://archive.softwareheritage.org",                
-#  "https://cn.dataone.org",                              
-#  "https://zenodo.org",                                  
-#  "/home/cboettig/.local/share/R/contentid"
+#  "https://hash-archive.org",                            
+  "https://hash-archive.carlboettiger.info",             
+  "https://archive.softwareheritage.org",                
+  "https://cn.dataone.org",                              
+  "https://zenodo.org",                                  
+  "/home/cboettig/.local/share/R/contentid",
   "")
   
   cols = c("source", "date")
