3c0086d8f05beb8061445668e871449a7338b19b
Carl <cboettig@gmail.com>
2021-11-18 23:22:28 +0000

    +dataone

--- R/test.R
+++ R/test.R
@@ -17,7 +17,7 @@ test <- function(){
 #  "https://hash-archive.org",                            
 #  "https://hash-archive.carlboettiger.info",             
   "https://archive.softwareheritage.org",                
-#  "https://cn.dataone.org",                              
+  "https://cn.dataone.org",                              
   "https://zenodo.org",                                  
   "/home/cboettig/.local/share/R/contentid",
   "")
