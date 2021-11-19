aa9353d9f95ff19d105783c62d0264ac61e81990
Carl <cboettig@gmail.com>
2021-11-19 00:46:00 +0000

    :see_no_evil:

--- R/test.R
+++ R/test.R
@@ -4,11 +4,12 @@
 #' @export
 test <- function(){
   id <- "hash://sha256/e17632191c0487128aa6d1ecb932b655157ad7eab26f2749c9797ce65bbbc130"
-  
   host <- "https://hash-archive.carlboettiger.info"
+  
   # contentid:::hash_archive_api(id, "api/sources", host) # hangs
-
-  status <- check_url(host)
+  endpoint <- "api/sources"
+  query <- id
+  status <- contentid:::check_url(host)
   if (status >= 400) 
     out <- data.frame()
   request <- paste(host, endpoint, query, sep = "/")  
