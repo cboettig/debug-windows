6a0152e07a96d1d68a9325343f52fa8004af74d4
Carl <cboettig@gmail.com>
2021-11-19 00:32:11 +0000

    check_url?

--- R/test.R
+++ R/test.R
@@ -6,9 +6,16 @@ test <- function(){
   id <- "hash://sha256/e17632191c0487128aa6d1ecb932b655157ad7eab26f2749c9797ce65bbbc130"
   
   host <- "https://hash-archive.carlboettiger.info"
-  contentid:::hash_archive_api(id, "api/sources", host)
+  # contentid:::hash_archive_api(id, "api/sources", host) # hangs
+  contentid:::check_url(host)
   
-  #contentid::resolve(id)
+  
+}
+
+
+
+test3 <- function(){  
+    
   request <- paste0("https://hash-archive.carlboettiger.info/api/sources/",id)
   result <- tryCatch({
     response <- httr::GET(request)
@@ -24,53 +31,3 @@ test <- function(){
   
 }
 
-
-test2 <- function(){
-  id <- "hash://sha256/e17632191c0487128aa6d1ecb932b655157ad7eab26f2749c9797ce65bbbc130"
-  
-  contentid:::sources_dataone(id)
-  contentid:::sources_zenodo(id)
-  contentid:::sources_swh(id)
-  contentid:::sources_store(id)
-  contentid:::sources_tsv(id)
-  
-  
-  registries <- c(
-    #  "/home/cboettig/.local/share/R/contentid/registry.tsv",
-    #  "https://hash-archive.org",                            
-    "https://hash-archive.carlboettiger.info",             
-    "https://archive.softwareheritage.org",                
-    "https://cn.dataone.org",                              
-    "https://zenodo.org",                                  
-    "/home/cboettig/.local/share/R/contentid",
-    "")
-  
-  cols = c("source", "date")
-  all = TRUE
-  
-  registries <- contentid:::expand_registry_urls(registries)
-  types <- contentid:::detect_registry_type(registries)
-  
-  active_registries <- registries[types == "content_store"]
-  out <- contentid:::generic_source(id, 
-                                    registries = active_registries, 
-                                    type = "content_store")
-  
-  if (all || all(is.na(out$source))) {
-    remote <- types[types %in% c(contentid:::MIXED, contentid:::REMOTES)]
-    remote_out <- lapply(
-      remote,
-      function(type) {
-        active_registries <- registries[types == type]
-        contentid:::generic_source(id,
-                                   registries = active_registries,
-                                   type = type
-        )
-      }
-    )
-    
-    remote_out <- do.call(rbind, remote_out)
-    out <- rbind(out, remote_out)
-  }
-  
-}
