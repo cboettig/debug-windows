2b7f841f235a035772a17cc57c18be2f4fabe0e5
Carl <cboettig@gmail.com>
2021-11-19 04:23:53 +0000

    this is weird

--- R/test.R
+++ R/test.R
@@ -9,7 +9,7 @@ test <- function(){
   # contentid:::hash_archive_api(id, "api/sources", host) # hangs
   endpoint <- "api/sources"
   query <- id
-#  status <- contentid:::check_url(host)
+  status <- contentid:::check_url(host)
 #  if (status >= 400) 
 #    out <- data.frame()
   request <- paste(host, endpoint, query, sep = "/")  
