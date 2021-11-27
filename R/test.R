94b9f24e8ee7395866d67930bea44fd0323f03c0
Carl Boettiger <cboettig@gmail.com>
2021-11-26 20:34:48 -0800

    not nobody

--- R/test.R
+++ R/test.R
@@ -8,7 +8,9 @@
 #' @export
 test <- function(){
   host <- "https://hash-archive.carlboettiger.info"
-  handle <- curl::new_handle(nobody = TRUE, customrequest = "GET")
+  handle <- curl::new_handle(
+    #nobody = TRUE, 
+    customrequest = "GET")
 #  handle <- curl::handle_setopt(handle, http09_allowed = TRUE)
   test <- curl::curl_fetch_memory(host, handle)
   response <- httr::GET(host)
