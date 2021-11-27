85a657cbae0a703acccd11f654b8cc2456efcf8e
Carl Boettiger <cboettig@gmail.com>
2021-11-26 17:06:10 -0800

    with no http09

--- R/test.R
+++ R/test.R
@@ -9,7 +9,7 @@
 test <- function(){
   host <- "https://hash-archive.carlboettiger.info"
   handle <- curl::new_handle(nobody = TRUE, customrequest = "GET")
-  handle <- curl::handle_setopt(handle, http09_allowed = TRUE)
+#  handle <- curl::handle_setopt(handle, http09_allowed = TRUE)
   test <- curl::curl_fetch_memory(host, handle)
   response <- httr::GET(host)
 }
